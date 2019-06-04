48144596d38e9e284941683f72ed7be38ac6079a
Kinda <lamkinun@gmail.com>
2019-06-04 16:28:43 +0800

    STEP 7

--- Step_7_predict.py
+++ Step_7_predict.py
@@ -2,8 +2,6 @@
 # Copyright (c) Microsoft Corporation and contributors. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for details.
 #
-from Step_1_import_data import import_data
-from Step_2_normalize_data import normalize_data
 
 
 def predict(X, model):
