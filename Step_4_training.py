ac79bd2ec1172365e4de4949e448e103543b0393
Kinda <lamkinun@gmail.com>
2019-06-04 16:26:21 +0800

    STEP 4

--- Step_4_training.py
+++ Step_4_training.py
@@ -8,10 +8,10 @@ def training(X, y, model):
     history = model.fit(
         x=X,
         y=y,
-        validation_split=0.2,
-        epochs=10,
-        batch_size=128,
-        verbose=1
+        validation_split=0.2,   # Separate data to training set and validation set.
+        epochs=10,              # Repeat times.
+        batch_size=128,         # Each epoch data input size.
+        verbose=1               # 0 = no log. 1 = log. 2 = log when epoch ending.
     )
 
     return model, history
